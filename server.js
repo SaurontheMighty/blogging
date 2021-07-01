d8f0ec1ba812cecb65b963738b31513ffdf6d4ce
Ashish Selvaraj <52637731+SaurontheMighty@users.noreply.github.com>
2021-07-01 19:27:58 -0400

    Routing and HTML

--- server.js
+++ server.js
@@ -1,16 +1,41 @@
  const http = require('http');
+ const fs = require('fs');
 
  const server = http.createServer((req, res) => {
-    console.log(req.method, req.url);
+    console.log(http.ClientRequest);
 
      // set content header
-    res.setHeader("Content-Type", 'text/plain');
+    res.setHeader("Content-Type", 'text/HTML');
+   
+    let path = './views/';
+    switch(req.url) {
+		case '/':
+			res.statusCode = 200;
+        	path += 'index.html';
+        	break;
+		case '/about':
+			res.statusCode = 200;
+			path += 'about.html';
+			break;
+		case '/blogs':
+			res.statusCode = 301;
+			res.setHeader('Location', '/about');
+			break;
+		default:
+			res.statusCode = 404;
+			path += '404.html';
+			break;
+    }
 
     // write the content
-    res.write("Hello Human!");
-    
-    // end
-    res.end();
+    fs.readFile(`${path}`, (err, data) => {
+      if(err) {
+        res.end("<p>ERROR! Cannot read HTML.</p>");
+      }
+      else {
+        res.end(data);
+      }
+    });
  });
 
  server.listen(3000, 'localhost', () => {
